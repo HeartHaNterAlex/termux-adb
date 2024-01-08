3a1cd000a9b02db01ffcf5c4dec53d496a56e43e
[]=Ālex=[] <139457526+HeartHaNterAlex@users.noreply.github.com>
2024-01-09 01:39:55 +0530

    Delete fb.sh
    update

--- fb.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Mz='fast';Gz=' ome';Oz=' dev';Fz='h';Iz='pyth';Bz='d +x';Jz='on s';Az='chmo';Lz='m-3';Ez='ll.s';Dz='sh_a';Pz='ices';Kz='yste';Hz='.sh';Nz='boot';Qz='./mm';Cz=' fla';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Az$Bz$Gz$Hz$z$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$z$Qz"
