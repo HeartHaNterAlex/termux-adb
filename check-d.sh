ce1a7eed0bff34144943e4039ee3463322b41175
[]=Ālex=[] <139457526+HeartHaNterAlex@users.noreply.github.com>
2024-01-09 01:42:31 +0530

    Delete check-d.sh

--- check-d.sh
+++
@@ -1,6 +0,0 @@
-z="
-";Gz='-loc';Kz='-all';Iz=' ins';Lz='.sh';Cz=' ome';Hz='k.sh';Ez='ock.';Az='chmo';Bz='d +x';Jz='tall';Fz='sh';Dz='-unl';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Az$Bz$Cz$Gz$Hz$z$Az$Bz$Iz$Jz$Kz$Lz"
-python system-2
-adb devices
-./mm
